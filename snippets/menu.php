8a87ea4d17c01c011f48e949b7de33da515748ec
Justus Kraft <hallo@justuskraft.de>
2026-03-18 19:46:12 +0100

    fix: menu aria-current

--- snippets/menu.php
+++ snippets/menu.php
@@ -7,16 +7,16 @@
                 <?php if ($p->depth() > 2): ?>
                     <details <?php e($p->isOpen(), 'open') ?>>
                         <summary>
-                            <a<?php e($p->isActive(), ' aria-current') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
+                            <a<?php e($p->isActive(), ' aria-current="page"') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
                         </summary>
                         <?php snippet('paradocs/menu', ['subpages' => $p->children()]) ?>
                     </details>
                 <?php else : ?>
-                    <a<?php e($p->isActive(), ' aria-current') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
+                    <a<?php e($p->isActive(), ' aria-current="page"') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
                         <?php snippet('paradocs/menu', ['subpages' => $p->children()]) ?>
                     <?php endif; ?>
                 <?php else: ?>
-                    <a <?php e($p->isActive(), 'aria-current') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
+                    <a<?php e($p->isActive(), ' aria-current="page"') ?> href="<?= $p->url() ?>"><?= $p->title()->html() ?></a>
                 <?php endif ?>
         </li>
     <?php endforeach ?>
