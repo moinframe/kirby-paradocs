047ab8813b5886cfe5b4aae2f656845fa96930a2
Justus Kraft <hallo@justuskraft.de>
2026-03-18 19:45:43 +0100

    fix: load all classes

--- index.php
+++ index.php
@@ -4,14 +4,30 @@ use Kirby\Cms\App as Kirby;
 use Kirby\Filesystem\F;
 
 F::loadClasses([
-    'Moinframe\\ParaDocs\\Parser' => 'src/Parser.php',
-    'Moinframe\\ParaDocs\\Routes' => 'src/Routes.php',
+    'Moinframe\\ParaDocs\\App' => 'src/App.php',
     'Moinframe\\ParaDocs\\Options' => 'src/Options.php',
+    'Moinframe\\ParaDocs\\Plugins' => 'src/Plugins.php',
+    'Moinframe\\ParaDocs\\Routes' => 'src/Routes.php',
+    'Moinframe\\ParaDocs\\Page\\IndexPage' => 'src/Page/IndexPage.php',
+    'Moinframe\\ParaDocs\\Page\\PluginPage' => 'src/Page/PluginPage.php',
+    'Moinframe\\ParaDocs\\Markdown\\Parser' => 'src/Markdown/Parser.php',
+    'Moinframe\\ParaDocs\\Markdown\\Processor' => 'src/Markdown/Processor.php',
+    'Moinframe\\ParaDocs\\Markdown\\ProcessorCollection' => 'src/Markdown/ProcessorCollection.php',
+    'Moinframe\\ParaDocs\\Markdown\\Processors\\AlertProcessor' => 'src/Markdown/Processors/AlertProcessor.php',
+    'Moinframe\\ParaDocs\\Markdown\\Processors\\CodeBlockProcessor' => 'src/Markdown/Processors/CodeBlockProcessor.php',
+    'Moinframe\\ParaDocs\\Markdown\\Processors\\RelativeImagesProcessor' => 'src/Markdown/Processors/RelativeImagesProcessor.php',
+    'Moinframe\\ParaDocs\\Highlighter\\Interface\\Highlighter' => 'src/Highlighter/Interface/Highlighter.php',
+    'Moinframe\\ParaDocs\\Highlighter\\Phiki' => 'src/Highlighter/Phiki.php',
+    'Moinframe\\ParaDocs\\Highlighter\\Simple' => 'src/Highlighter/Simple.php',
 ], __DIR__);
 
 Kirby::plugin('moinframe/paradocs', [
     'options' => [
         'cache' => true
+    ],
+    'pageModels' => [
+        'paradocs-index' => Moinframe\ParaDocs\Page\IndexPage::class,
+        'paradocs-plugin' => Moinframe\ParaDocs\Page\PluginPage::class,
     ],
     'routes' => Moinframe\ParaDocs\Routes::register(),
     'templates' => [
